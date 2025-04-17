dca7c8570e6785873120e221bb2806bc031b2553
AlvaroRevi <100405496@alumnos.uc3m.es>
2025-04-18 00:36:01 +0200

    Agrega mensajes motivacionales para jóvenes en el archivo basico.py

--- basico.py
+++ basico.py
@@ -5,3 +5,7 @@ if edad >= 18:
     print("Eres mayor de edad")
 else:
     print("Eres menor de edad")
+    print("¡Pero no te preocupes!")
+    print("La vida es bella a cualquier edad")
+    print("Disfruta tu juventud y sigue aprendiendo")
+    print("🌟 ¡El futuro es brillante! 🌟")
