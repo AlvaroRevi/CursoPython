02a164aaeaebb593825b73e8a4bc673ae795619a
Alvaro Reviriego <alvaro.reviriego@idaerosolutions.com>
2025-04-21 08:48:11 +0200

    Se agrega un ejemplo de bucle for al archivo basico.py

--- basico.py
+++ basico.py
@@ -5,6 +5,13 @@ if edad >= 18:
     print("Eres mayor de edad")
 else:
     print("Eres menor de edad")
-    print("¡Pero no te preocupes!")
+    print("¡Pero no te preocupes!")     
 
 
+# Ejemplo de bucle for
+for i in range(5):
+    print(f"Iteración {i}")
+
+
+
+                
